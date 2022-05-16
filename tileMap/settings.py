9a2e33ffc005b5c87630bc6d8f6d36ab8372a89c
Quinten <89473193+Quinten-White-101@users.noreply.github.com>
2022-05-16 08:51:59 -0600

    Add files via upload

---
+++ tileMap/settings.py
@@ -0,0 +1,23 @@
+#settings
+#sets the color
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# game window settings
+WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
+HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
+FPS = 60
+TITLE = "Tilemap Demo"
+BGCOLOR = DARKGREY
+
+TILESIZE = 16
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+#player settings
+PLAYER_SPEED = 250
