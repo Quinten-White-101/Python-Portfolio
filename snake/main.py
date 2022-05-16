8be791c56fee7d449ba5a5795cac7d7a47006892
Quinten <89473193+Quinten-White-101@users.noreply.github.com>
2022-05-16 09:00:57 -0600

    Add files via upload

---
+++ snake/main.py
@@ -0,0 +1,27 @@
+#Quinten White
+#Snake Game
+#2/28/2022
+#IMPORTS
+import sys
+import random
+from PIL import Image, ImageTk
+from tkinter import Tk, Frame, Canvas, ALL,NW
+from board import *
+
+
+class Snake(Frame):
+    def __init__(self):
+        super(Snake,self).__init__()
+        self.master.title("Snake")
+        self.board = Board()
+        self.pack()
+
+
+
+def main():
+    root = Tk()
+    nib = Snake()
+    root.mainloop()
+
+main()
+
